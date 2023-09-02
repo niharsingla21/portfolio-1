07cf05164b6ff0e71e07b3bb479948ccc24fd545
Nihar Singla <nihar.singla@measured.com>
2023-09-02 18:12:23 +0530

    WIP on Experience.js

--- src/Pages/Experience.js
+++ src/Pages/Experience.js
@@ -11,18 +11,20 @@ export default function Experience() {
                 <Grid container height="100%" maxWidth="100%" maxHeight="100%">
                     <Grid container item xs="3" md="4" lg="4" height="100%" width="100%" padding="20px 20px 20px 20px">
                         <Box style={{
-                            outline: `1px solid white`,
                             height:"100%",
-                            width:"100%"
+                            width:"100%",
+                            border:"1px solid white",
+                            borderRadius:"10px"
                         }}>
                             TIMELINE
                         </Box>
                     </Grid  >
-                    <Grid container item xs="9" md="8" lg="8" height="100%" width="100%">
+                    <Grid container item xs="9" md="8" lg="8" height="100%" width="100%" padding="20px 20px 20px 20px">
                         <Box style={{
-                            outline: `2px solid white`,
                             height:"100%",
-                            width:"100%"
+                            width:"100%",
+                            border:"1px solid white",
+                            borderRadius:"10px"
                         }}>
                             <Grid item>Exp 1</Grid>
                             <Grid item>Exp 2</Grid>

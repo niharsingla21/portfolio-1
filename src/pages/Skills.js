9239ceb03cc440b663287d01c8025ec672e3559b
Nihar <nihar.singla21@gmail.com>
2023-09-25 23:47:50 +0530

    created skills.json

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,12 +1,27 @@
 import BasePage from "../components/BasePage";
-import {Container} from "@mui/material";
+import {Container, Grid} from "@mui/material";
+import {Masonry} from "@mui/lab";
 
 export default function Skills() {
 
     const getPageContent = () => {
         return (
             <Container maxWidth={"lg"} className="height-100">
-
+                <Grid container height="100%" maxWidth="100%" maxHeight="100%" padding="60px 20px">
+                    <Masonry
+                        columns={4}
+                        spacing={2}
+                        defaultHeight={450}
+                        defaultColumns={4}
+                        defaultSpacing={1}
+                    >
+                        {skills.map((height, index) => (
+                            <Item key={index} sx={{ height }}>
+                                {index + 1}
+                            </Item>
+                        ))}
+                    </Masonry>
+                </Grid>
             </Container>
         )
     }
